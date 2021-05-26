29a468c7db2c79e90c1cef05141a0cbff66469f2
MelTripot <tripot.melanie@ynov.com>
2021-05-26 10:48:48 +0200

    Recup modif

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  
+  public void PlayGame()
+    {
+        // Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+}
